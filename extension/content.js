c071f568bc4e17b28d53f476fae17b566df19d95
Kieran Llarena <baymax709@gmail.com>
2024-05-11 15:52:37 -0400

    feat(extension): captured ump chunks

--- extension/content.js
+++ extension/content.js
@@ -1,23 +1,35 @@
-const ws = new WebSocket("ws://127.0.0.1:8080");
+// const ws = new WebSocket("ws://127.0.0.1:8080");
 
-let titles;
+// let titles;
 
-ws.addEventListener("open", () => {
-  titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
+// ws.addEventListener("open", () => {
+//   titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
 
-  const observer = new PerformanceObserver((list) => {
-    list.getEntries().forEach((entry) => {
-      if (entry.name.includes('https://yt3.ggpht.com/')) {
-        titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
+//   const observer = new PerformanceObserver((list) => {
+//     list.getEntries().forEach((entry) => {
+//       if (entry.name.includes('https://yt3.ggpht.com/')) {
+//         titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
 
-        ws.send(`${titles}`);
-      }
-    });
-  });
+//         ws.send(`${titles}`);
+//       }
+//     });
+//   });
   
-  observer.observe({ type: "resource", buffered: true });
+//   observer.observe({ type: "resource", buffered: true });
+// });
+
+// ws.addEventListener("close", () => ws.close());
+
+// ws.addEventListener("error", err => console.log(err));
+
+// initiatorType = "fetch"
+// 
+
+const observer = new PerformanceObserver((list) => {
+  list.getEntries().forEach((entry) => {
+    if (entry.initiatorType === "fetch" && entry.name.includes(".googlevideo.com/videoplayback?")) console.log(entry);
+  });
 });
 
-ws.addEventListener("close", () => ws.close());
-
-ws.addEventListener("error", err => console.log(err));
+// observer.observe({ type: "resource", buffered: true });
+observer.observe({ entryTypes: ['resource'] });
