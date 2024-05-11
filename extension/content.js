bf899bf2db3785cf23590ea8a7a75506e8f3393a
Kieran Llarena <baymax709@gmail.com>
2024-05-11 16:01:12 -0400

    feat(extension): send video playback url via ws

--- extension/content.js
+++ extension/content.js
@@ -1,35 +1,17 @@
-// const ws = new WebSocket("ws://127.0.0.1:8080");
+const ws = new WebSocket("ws://127.0.0.1:8080");
 
-// let titles;
-
-// ws.addEventListener("open", () => {
-//   titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
-
-//   const observer = new PerformanceObserver((list) => {
-//     list.getEntries().forEach((entry) => {
-//       if (entry.name.includes('https://yt3.ggpht.com/')) {
-//         titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
-
-//         ws.send(`${titles}`);
-//       }
-//     });
-//   });
-  
-//   observer.observe({ type: "resource", buffered: true });
-// });
-
-// ws.addEventListener("close", () => ws.close());
-
-// ws.addEventListener("error", err => console.log(err));
-
-// initiatorType = "fetch"
-// 
-
-const observer = new PerformanceObserver((list) => {
-  list.getEntries().forEach((entry) => {
-    if (entry.initiatorType === "fetch" && entry.name.includes(".googlevideo.com/videoplayback?")) console.log(entry);
+ws.addEventListener("open", () => {
+  const observer = new PerformanceObserver((list) => {
+    list.getEntries().forEach((entry) => {
+      if (entry.initiatorType === "fetch" && entry.name.includes(".googlevideo.com/videoplayback?")) {
+        ws.send(entry.name);
+      }
+    });
   });
+  
+  observer.observe({ entryTypes: ['resource'] });
 });
 
-// observer.observe({ type: "resource", buffered: true });
-observer.observe({ entryTypes: ['resource'] });
+ws.addEventListener("close", () => ws.close());
+
+ws.addEventListener("error", err => console.log(err));
