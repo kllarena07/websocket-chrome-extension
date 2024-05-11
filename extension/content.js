a8d618b77d5005d277385631dcf6aa3b3e19df5c
Kieran Llarena <baymax709@gmail.com>
2024-05-10 22:10:47 -0400

    feat(extension): send all video titles to websocket server

--- extension/content.js
+++ extension/content.js
@@ -1,10 +1,16 @@
 const ws = new WebSocket("ws://127.0.0.1:8080");
 
+let titles;
+
 ws.addEventListener("open", () => {
+  titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
+
   const observer = new PerformanceObserver((list) => {
     list.getEntries().forEach((entry) => {
       if (entry.name.includes('https://yt3.ggpht.com/')) {
-        ws.send(entry.name);
+        titles = Array.from(document.querySelectorAll('yt-formatted-string#video-title'), element => element.innerText);
+
+        ws.send(`${titles}`);
       }
     });
   });
