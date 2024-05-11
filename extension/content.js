5ab04109db4f161bf741a1fe15188adc6e098a92
Kieran Llarena <baymax709@gmail.com>
2024-05-10 21:27:56 -0400

    chore(frontend): code for future proj

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+// Code for a future project. Capture all loading channel icons
+// const observer = new PerformanceObserver((list) => {
+//   list.getEntries().forEach((entry) => {
+//     if (entry.name.includes('https://yt3.ggpht.com/')) console.log(entry.name);
+//     // const request = entry.responseStart - entry.requestStart;
+//     // if (request > 0) {
+//     //   console.log(`${entry.name}: Request time: ${request}ms`);
+//     // }
+//   });
+// });
+
+// observer.observe({ type: "resource", buffered: true });
+
