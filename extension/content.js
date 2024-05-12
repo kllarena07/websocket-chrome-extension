5aeec876767dde1bf34a4da1c10eeae865662582
Kieran Llarena <baymax709@gmail.com>
2024-05-11 20:17:54 -0400

    revert(extension): going with original listening for chunks method

--- extension/content.js
+++ extension/content.js
@@ -1,20 +1,18 @@
-// Commenting out because this extracts data in a format that needs some extra research on
+const ws = new WebSocket("ws://127.0.0.1:8080");
 
-// const ws = new WebSocket("ws://127.0.0.1:8080");
-
-// ws.addEventListener("open", () => {
-//   const observer = new PerformanceObserver((list) => {
-//     list.getEntries().forEach((entry) => {
-//       if (entry.initiatorType === "fetch" && entry.name.includes(".googlevideo.com/videoplayback?")) {
-//         console.log(entry);
-//         ws.send(entry.name);
-//       }
-//     });
-//   });
+ws.addEventListener("open", () => {
+  const observer = new PerformanceObserver((list) => {
+    list.getEntries().forEach((entry) => {
+      if (entry.initiatorType === "fetch" && entry.name.includes(".googlevideo.com/videoplayback?")) {
+        console.log(entry);
+        ws.send(entry.name);
+      }
+    });
+  });
   
-//   observer.observe({ entryTypes: ['resource'] });
-// });
+  observer.observe({ entryTypes: ['resource'] });
+});
 
-// ws.addEventListener("close", () => ws.close());
+ws.addEventListener("close", () => ws.close());
 
-// ws.addEventListener("error", err => console.log(err));
+ws.addEventListener("error", err => console.log(err));
